5ca44cc624e4ae378dc2024eef39d76347eb3a19
mramos <mario.ramos@lastminute.com>
2016-03-27 12:04:00 +0200

    Red add two product to scan

--- src/test/java/checkouit/ScanTest.java
+++ src/test/java/checkouit/ScanTest.java
@@ -7,14 +7,25 @@ import org.junit.Test;
 
 public class ScanTest
 {
-
   @Test
-  public void scanProduct(){
+  public void whenScanProductAThenReturnPriceA(){
     Checkout checkout = new Checkout();
     Product product = new Product("A");
     checkout.scan(product);
     
-    Price totalExpected= new Price(0.0) ;
+    Price totalExpected= new Price(50.0) ;
+    assertThat(checkout.total(), equalTo(totalExpected));
+  }  
+  
+  @Test
+  public void whenScanProductAandBThenReturnPriceAMoreB(){
+    Checkout checkout = new Checkout();
+    Product productA = new Product("A");
+    checkout.scan(productA);
+    Product productB = new Product("B");
+    checkout.scan(productB);
+    
+    Price totalExpected= new Price(80.0) ;
     assertThat(checkout.total(), equalTo(totalExpected));
   }
 }
